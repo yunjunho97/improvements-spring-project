d3269a3cdfbfdf24d6cc14aaee7406f5f73c3ca7
yunjunho97 <yunjunho97.gm@gmail.com>
2024-08-12 09:50:43 +0900

    setting

---
+++ src/main/java/com/example/improvements/ImprovementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.improvements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImprovementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/improvements/ImprovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.improvements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
